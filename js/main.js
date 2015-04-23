449d8af707f24936b0445a8fbba15edf8a15a8bb
Phil London <philip.n.london@gmail.com>
2015-04-23 08:31:11 -0400

    start from scratch with tests

--- js/main.js
+++ js/main.js
@@ -1,188 +1,65 @@
-var Game = function() {
-	this.$body = $('body');
-	this.$modal = $('.modal');
-	this.$board = $('.board');
-	this.$cells = $('.board li');
-	this.$clonedCells = this.$cells.clone();
-	this.first = true;
-	this.w = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[6, 4, 2]
-	];
+var emptyBoard = ["","","",
+									"","","",
+									"","",""];
 
-	this.init = function(e) {
-		var $this = $(e.currentTarget);
-		this.$body.removeClass('overlay');
+var winningBoard = ["x","x","x",
+										"o","x","o",
+										"x","o","o"];
 
-		if ( $this.index() === 0 ) {
-			this.computer = 'x';
-			this.player = 'o';
-			this.compMove();
-		} else {
-			this.player = 'x';
-			this.computer = 'o';
-		}
-	};
+var drawBoard = ["x","o","x",
+                 "o","x","o",
+                 "o","x","o"];
 
-	this.humanMove = function(e) {
-		if ( this.busy ) { return; }
-		var $this = $(e.currentTarget);
+var winningCombos = [
+ 										 //horizontal
+                     [0,1,2], 
+                     [3,4,5],
+                     [6,7,8],
+                     // vertical
+                     [0,3,6],
+                     [1,4,7],
+                     [2,5,8],
+                     // diagonal
+                     [0,4,8],
+                     [2,4,6]];
 
-		if ( $this.is(':empty') ) {
-			$this.text(this.player);
-			this.first = false;
-			this.compMove();
-		} else {
-			return false;
-		}
-	};
+var gameOver = function (board) {
+	results = [];
+	winningCombos.forEach(function (combo) {
+		var result = '';
+		for (var i = 0; i < 3; i++) {
+			result += board[combo[i]];
+		};
+		results.push(result);
+	});
 
-	this.compMove = function() {
-		var idx;
-		var self = this;
-		this.busy = true;
-		if (this.first) {
-			idx = [0, 2, 6, 8][Math.floor(Math.random()*4)];
-		} else if (this.fatal()) {
-			idx = this.fatal(this.computer);
-		} else {
-			idx = this.bestMove(this.$cells);
-		}
+	if ( results.indexOf('xxx') != -1 || 
+		   results.indexOf('ooo') != -1 ) {
+		return 10;
+	} else if (board.join("").length === 9) {
+		return 0;
+	}
+	else {
+		return false;
+	}
+}
 
-		this.populate(idx, this.computer);
-		setTimeout(function() { self.gameOver(); }, 1000);
-		this.busy = false;
-	};
-	this.fatal = function(player) {
-		// win
-		for ( var i = 0; i < 8; i++ ) {
-			var x = [];
-			for ( var j = 0; j < 3; j++ ) {
-				x.push(this.$cells[this.w[i][j]]);
-			}
-			var e = x.filter(function(cell) {
-				return $(cell).is(':empty');
-			});
-			var f = x.filter(function(cell) {
-				return !$(cell).is(':empty');
-			});
-			if ( e.length == 1 &&
-					 this.computer == $(f).first().text() &&
-				   this.computer == $(f).last().text()) {
-				this.populate($(e).index(), this.computer);
-				return true;
-			}
-		}
-		// defend
-		for ( var i = 0; i < 8; i++ ) {
-			var x = [];
-			for ( var j = 0; j < 3; j++ ) {
-				x.push(this.$cells[this.w[i][j]]);
-			}
-			var e = x.filter(function(cell) {
-				return $(cell).is(':empty');
-			});
-			var f = x.filter(function(cell) {
-				return !$(cell).is(':empty');
-			});
-			if (e.length == 1 &&
-					 this.player == $(f).first().text() &&
-				   this.player == $(f).last().text()) {
-				this.populate($(e).index(), this.computer);
-				return true;
-			}
-		}
-	};
+var computerPlayer = 0;
+var currentPlayer = 1;
 
-	this.bestMove = function($cells) {
-		var self = this;
-		var bestMove;
+this.currentPlayer = currentPlayer;
 
-		$cells = $cells.filter(function() {
-			return $(this).is(':empty');
-		});
+var minimax = function(board) {
+	if ( gameOver(board) ) {
+		var score = this.currentPlayer === 1 ? gameOver(board) : -gameOver(board);
+	}
+	return score;
+}
 
-		if (this.fork() || !$(this.$cells[7]).is(':empty')) {
-			$(this.$cells[1]).data('score', 40);
-			$(this.$cells[6]).data('score', 30);
-			$(this.$cells[8]).data('score', 30);
-		}
+// tests
+console.log(gameOver(emptyBoard) != 10);
+console.log(gameOver(winningBoard) === 10);
+console.log(gameOver(drawBoard) === 0);
 
-		$cells.sort(function(a,b) {
-			return ($(a).data('score')) < ($(b).data('score')) ? 1 : -1;    
-		});
-
-		return $cells.first().index();
-	};
-
-	this.fork = function() {
-		for ( var i = 0; i < 8; i++ ) {
-			var x = [];
-			for ( var j = 0; j < 3; j++ ) {
-				x.push(this.$cells[this.w[i][j]]);
-			}
-			var f = x.filter(function(cell) {
-				return !$(cell).is(':empty');
-			}).map(function(cl) {
-				return $(cl).text();
-			});
-			console.log(f.toString());
-			if (f.toString() == ["x", "o", "x"].toString()) {
-				return true;
-			}
-		}
-	};
-
-	this.populate = function(idx, player) {
-		var self = this;
-		setTimeout(function() {
-			$(self.$cells[idx]).text(player);
-		}, 500);
-	};
-
-	this.gameOver = function() {
-		var self = this;
-		for ( var i = 0; i < 8; i++ ) {
-			var x = [];
-			for ( var j = 0; j < 3; j++ ) {
-				x.push(this.$cells[this.w[i][j]]);
-			}
-			var f = x.filter(function(cell) {
-				return !$(cell).is(':empty');
-			});
-			if ( f.length == 3 &&
-					 $(f[0]).text() === this.computer &&
-					 $(f[1]).text() === this.computer &&
-					 $(f[2]).text() === this.computer) {
-				$(f).addClass('winner');
-				setTimeout(function() {
-					alert('Computer Wins');
-					self.winner = true;
-					self.playAgain();
-				}, 1050);
-			}
-		}
-		var draw = this.$cells.filter(function() {
-			return !$(this).is(':empty');
-		});
-		if (draw.length == 9 && !this.winner) {
-			this.$cells.addClass('draw');
-			setTimeout(function() {
-				alert('It\'s a Draw');
-				self.playAgain();
-			}, 1050);
-		}
-	};
-
-	this.playAgain = function() {
-		this.$body.addClass('overlay');
-		window.location.reload();
-	};
-};
-
+console.log('')
+console.log(minimax(winningBoard) === 10)
