8c2f4ae6d3d6b0bc23616712aec46aa2f6c55ee0
Phil London <philip.n.london@gmail.com>
2015-04-25 13:10:44 -0400

    minimax implemented

---
+++ js/boards.js
@@ -0,0 +1,20 @@
+var emptyBoard = ["","","",
+									"","","",
+									"","",""];
+
+var winningBoard = ["x","x","x",
+										"o","x","o",
+										"x","o","o"];
+
+var drawBoard = ["x","o","x",
+                 "o","x","o",
+                 "o","x","o"];
+
+
+var oneMoreToWin = ["o","x","o",
+                    "x","x","o",
+                    "x","o",""];
+
+var twoMoreToWin = ["","","x",
+                    "x","o","o",
+                    "o","x","x"];
--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
-var emptyBoard = ["","","",
-									"","","",
-									"","",""];
+window.activePlayer = 'x';
+window.inactivePlayer = 'o';
 
-var winningBoard = ["x","x","x",
-										"o","x","o",
-										"x","o","o"];
-
-var drawBoard = ["x","o","x",
-                 "o","x","o",
-                 "o","x","o"];
+this.turn = 'x';
 
 var winningCombos = [
  										 //horizontal
@@ -29,37 +22,101 @@ var gameOver = function (board) {
 		var result = '';
 		for (var i = 0; i < 3; i++) {
 			result += board[combo[i]];
-		};
+		}
 		results.push(result);
 	});
 
-	if ( results.indexOf('xxx') != -1 || 
-		   results.indexOf('ooo') != -1 ) {
+	if ( results.indexOf('xxx') != -1 ) {
+		this.winner = 'x';
+		return 10;
+	} else if ( results.indexOf('ooo') != -1 ) {
+		this.winner = 'o';
 		return 10;
 	} else if (board.join("").length === 9) {
 		return 0;
-	}
-	else {
+	} else {
 		return false;
 	}
-}
+};
 
-var computerPlayer = 0;
-var currentPlayer = 1;
-
-this.currentPlayer = currentPlayer;
+this.choices = [];
+this.moves = [];
+var choice;
+var freq;
+var arr;
 
 var minimax = function(board) {
-	if ( gameOver(board) ) {
-		var score = this.currentPlayer === 1 ? gameOver(board) : -gameOver(board);
+	if ( gameOver(board) || gameOver(board) === 0 ) {
+		var score = this.winner === window.activePlayer ? gameOver(board) : -gameOver(board);
+		return score;
 	}
-	return score;
-}
 
-// tests
-console.log(gameOver(emptyBoard) != 10);
-console.log(gameOver(winningBoard) === 10);
-console.log(gameOver(drawBoard) === 0);
+	if ( this.turn === window.activePlayer ) {
+		freq = {};
+		this.choices.forEach(function(choice) {
+			freq[choice[1]] = freq[choice[1]] === undefined ? 1 :  freq[choice[1]]+=1;
+		});
 
-console.log('')
-console.log(minimax(winningBoard) === 10)
+		arr = Object.keys( freq ).map(function ( key ) { return freq[key]; });
+
+		max = Math.max.apply(null, arr);
+
+		for (var i in freq) {
+			if ( freq[i] === max) {
+				choice = parseInt(i);
+			}
+		}
+	} else {
+		freq = {};
+		this.choices.forEach(function(choice) {
+			freq[choice[1]] = freq[choice[1]] === undefined ? 1 :  freq[choice[1]]+=1;
+		});
+
+		arr = Object.keys( freq ).map(function ( key ) { return freq[key]; });
+
+		min = Math.max.apply(null, arr);
+
+		for (var i in freq) {
+			if ( freq[i] === min) {
+				choice = parseInt(i);
+			}
+		}
+	}
+
+	getPossibleMoves(board).forEach(function(move) {
+		var game = getGameState(board, move);
+		var result = minimax(game);
+
+		if ( result ) {
+			this.choices.push([result, move, this.turn]);
+		}
+	});
+
+};
+
+var getPossibleMoves = function(board) {
+	for (var a=[],i=board.length;i--;) if (board[i]==="") a.push(i);
+	return a.reverse();  
+};
+
+var getGameState = function(board, idx) {
+	var clone = board.slice(0);
+
+	if ( !this.second ) {
+		this.turn = window.activePlayer;
+	} else {
+		if ( this.turn === window.activePlayer ) {
+			this.turn = window.inactivePlayer;
+		} else {
+			this.turn = window.activePlayer;
+		}
+	}
+
+	this.second = true;
+
+	clone[idx] = this.turn;
+	return clone;
+};
+
+minimax(twoMoreToWin);
+console.log(choice, window.activePlayer);
